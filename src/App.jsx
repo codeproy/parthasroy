7d7b2c790cb2fd320b641afd16e36d2d38823b90
PARTHA SARATHY ROY <roypsr@PARTHAs-MacBook-Air.local>
2026-04-14 23:55:45 -0400

    Home page route

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <Header />
         <main className="main-content">
           <Routes>
-            <Route path="/parthasroy" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </main>
